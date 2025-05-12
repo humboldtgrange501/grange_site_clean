14d6952a2276bb8bd3371d591b5f01adf6178c74
Lily Yassemi <501.humboldt.grange@gmail.com>
2025-05-11 19:14:29 -0700

    changed .env location

--- grange_site/src/SupabaseClient.js
+++ grange_site/src/SupabaseClient.js
@@ -1,6 +1,9 @@
 // src/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
+console.log('URL:', process.env.REACT_APP_SUPABASE_URL);
+console.log('KEY:', process.env.REACT_APP_SUPABASE_ANON_KEY);
+
 const supabase = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_ANON_KEY
