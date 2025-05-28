cf3dbbace7f0c09b419bebfccf7e824c7e99465b
humboldtgrange501 <501.humboldt.grange@gmail.com>
2025-05-27 21:31:41 -0700

    asdfghjkl

--- grange_site/netlify/functions/heartbeat/heartbeat.mjs
+++ grange_site/netlify/functions/heartbeat/heartbeat.mjs
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js'
 const REACT_APP_SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL
 const REACT_APP_SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-const supabase = createClient(REACT_APP_SUPABASE_URL, SUPABASE_ANON_KEY)
+const supabase = createClient(REACT_APP_SUPABASE_URL, REACT_APP_SUPABASE_ANON_KEY)
 
 export default async () => {
   try {
